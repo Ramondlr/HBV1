577d4f8b36d947fa1024ec5c52174325cc228b1c
Ramondlr <42787384+Ramondlr@users.noreply.github.com>
2021-11-24 09:46:04 +0000

    Update RecipeService.java

--- Recipe/src/main/java/is/hi/recipe/Services/RecipeService.java
+++ Recipe/src/main/java/is/hi/recipe/Services/RecipeService.java
@@ -13,7 +13,7 @@ public interface RecipeService {
     Recipe save(Recipe recipe);
     void delete(Recipe recipe);
     void deleteRecipeImage(Recipe recipe);
-    public boolean hasUserRecipe(List<Recipe> allRecipes, long id);
+    boolean hasUserRecipe(List<Recipe> allRecipes, long id);
 
     @Query(value = "select * from recipes r where r.recipe_Title like '%keyword%' or r.recipe_text like '%keyword%' or r.recipe_tag like '%keyword%'", nativeQuery = true)
     List<Recipe> findByKeyword(@Param("keyword") String keyword);
