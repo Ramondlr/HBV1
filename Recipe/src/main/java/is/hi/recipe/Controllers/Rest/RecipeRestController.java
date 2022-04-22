2796ce86572aa2c96101c732081152dabd5e8d61
hrolfurj <hrolfur@hi.is>
2022-04-22 01:35:52 +0000

    tiltekt

--- Recipe/src/main/java/is/hi/recipe/Controllers/Rest/RecipeRestController.java
+++ Recipe/src/main/java/is/hi/recipe/Controllers/Rest/RecipeRestController.java
@@ -1,18 +1,10 @@
 package is.hi.recipe.Controllers.Rest;
 
-import is.hi.recipe.Controllers.Rest.Request.UserRequest;
 import is.hi.recipe.Controllers.Rest.Response.RecipeResponse;
-import is.hi.recipe.Controllers.Rest.Response.UserResponse;
 import is.hi.recipe.Persistence.Entities.Recipe;
-import is.hi.recipe.Persistence.Entities.User;
 import is.hi.recipe.Services.RecipeService;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -47,8 +39,6 @@ public class RecipeRestController {
         }
         recipeService.save(recipe);
         return new RecipeResponse(recipeService.findUserRecipes(recipe.getUserID()));
-        //return new RecipeResponse(recipeService.findByID(recipe.getID()));
-        //return null;
     }
 
     @GetMapping("/api/{id}/deleteRecipe")
@@ -57,31 +47,5 @@ public class RecipeRestController {
         Recipe recipe = recipeService.findByID(id);
         recipeService.delete(recipe);
         return new RecipeResponse(recipeService.findUserRecipes(recipe.getUserID()));
-        //return new RecipeResponse(recipeService.findByID(recipe.getID()));
-        //return null;
     }
-
-    /*
-    @GetMapping(value = "/delete/{id}")
-    public void deleteRecipe(@PathVariable("id") long id){
-
-    @RequestMapping(value = "/api/recipe/{id}")
-    public Recipe getRecipe(@PathVariable("id") long id) throws InterruptedException {
-
-        Recipe recipes = recipeService.findByID(id);
-
-        return recipes;
-    }
-
-
-    @RequestMapping(value = "/api/(delete/{id}")
-    public boolean deleteRecipe(@PathVariable("id") long id){
->>>>>>> 65564254f8deec1edf73a75b9012c16de7c58fed
-        //Business Logic is always on the service that's why we let the recipeService do all the work
-        Recipe recipeToDelete = recipeService.findByID(id);
-        recipeService.delete(recipeToDelete);
-
-        return recipeService.findByID(id) != null;
-    }*/
-
 }
--- Recipe/src/main/java/is/hi/recipe/Controllers/Rest/UserRestController.java
+++ Recipe/src/main/java/is/hi/recipe/Controllers/Rest/UserRestController.java
@@ -1,19 +1,14 @@
 package is.hi.recipe.Controllers.Rest;
 
 import is.hi.recipe.Controllers.Rest.Request.UserRequest;
-import is.hi.recipe.Controllers.Rest.Response.RecipeResponse;
 import is.hi.recipe.Controllers.Rest.Response.UserResponse;
 import is.hi.recipe.Persistence.Entities.User;
 import is.hi.recipe.Services.RecipeService;
 import is.hi.recipe.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserRestController {
@@ -30,9 +25,7 @@ public class UserRestController {
     @PostMapping("/api/login")
     @ResponseBody
     public UserResponse logIn (@RequestBody UserRequest userRequest) {
-
         User user = userService.login(new User(userRequest.getUsername(), userRequest.getPassword()));
-
         return new UserResponse(user);
     }
 
@@ -40,8 +33,6 @@ public class UserRestController {
     @ResponseBody
     public UserResponse isLoggedIn (@RequestBody UserRequest userRequest) {
         User user = userService.findByUsername(userRequest.getUsername());
-        String uss = user.getPassword();
-        String usss = userRequest.getPassword();
         if (user.getPassword().compareTo(userRequest.getPassword()) == 0) {
             return new UserResponse(user);
         }
@@ -57,7 +48,6 @@ public class UserRestController {
             throw new ResponseStatusException(HttpStatus.IM_USED);
         }
         if (userName == "" || pwd == "") {
-            //return null;
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE);
         }
         User user = userService.signUp(userRequest.getUsername(), userRequest.getPassword());
@@ -84,55 +74,4 @@ public class UserRestController {
 
         return new UserResponse(user);
     }
-
-
-
-    /*
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void loginPOST(User user){
-
-        userService.login(user);
-    }
-
-     */
-
-
-    /*
-
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String signupGET(User user){
-        return "signup";
-    }
-
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signupPOST(User user, BindingResult result){
-        if(result.hasErrors()){
-            return "redirect:/signup";
-        }
-
-        User exists = userService.findByUsername(user.getUsername());
-
-        // Koma með almennilegt villuskilaboð, seinna meir.
-        // Skoðar ef notandanafn er núþegar á skrá þegar nýr notandi er að reyna að sign up
-        if(exists != null){
-            return "redirect:/signup";
-        }
-        userService.save(user);
-
-        // TODO búa til if condition til að checka ef exists != null þ.e.a.s. ef það er núþegar til user með sama
-        //  username að meðhöndla þá villu.
-
-        if(exists == null){
-            userService.save(user);
-        }
-        return "redirect:/";
-    }
-     */
-/**
-    @GetMapping("/api/{id}/recipeList")
-    @ResponseBody
-    public RecipeResponse recipeList(@PathVariable("id") Long id) {
-        return new RecipeResponse(recipeService.findUserRecipes(id));
-    }
-*/
 }
